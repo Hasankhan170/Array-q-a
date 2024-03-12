27de37d17ed5343262647ab71d0215e37232484e
Hasankhan170 <hasankhanali0316@gmail.com>
2024-03-11 17:54:17 -0700

    arrays Queastion

---
+++ app.js
@@ -0,0 +1,39 @@
+// Q.1 
+// let userName = [""];
+// DONE 
+
+// Q.2 
+// let userName = new Array();
+// DONE 
+
+// Q.3
+// let userName = ["string"]
+// done 
+
+// Q.4
+// let userName = [10]
+// done 
+
+// Q.5
+// let userName = [true,false]
+// done
+
+// Q.6
+// let userName = ["string" , 10 , true];
+// done 
+
+// Q.7
+// let userName = ['available in moible market']
+// done 
+
+// Q.8
+// var para = document.querySelector('p')
+// var ul = document.querySelector('ul,li')
+// let userName = ['available mobile network in pakistan']
+// para.innerHTML= p
+// ul.innerHTML =ul,li
+// done 
+
+
+// Q.9
+const store = ['Top movies of 2015']
