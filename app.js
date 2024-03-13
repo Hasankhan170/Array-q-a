55a114c1cbd39532e13268d84b51979bbc9383a8
Hasankhan170 <hasankhanali0316@gmail.com>
2024-03-13 10:35:24 -0700

    index file

--- app.js
+++ app.js
@@ -36,4 +36,179 @@
 
 
 // Q.9
-const store = ['Top movies of 2015']
+// var TopMoviesOf2015 = ['']
+// TopMoviesOf2015.push("1)avengers");
+// TopMoviesOf2015.push("2)astro martin");
+// TopMoviesOf2015.push("3)inside out");
+// TopMoviesOf2015.push("4)spectre");
+
+// document.write("The Top 5 movies Of 2015 Are :" +TopMoviesOf2015.join("<br>"));
+// document.write("<br>")
+// document.write("the total movies in the lists is :" +TopMoviesOf2015.length)
+// DONE 
+
+
+// Q.10
+// var cars = ["buggati" , "supra" , "lambo" , "civic" , "grande"]
+
+// document.write(cars)
+// document.write("<br>")
+
+// document.write("the first index of the array :" +cars.indexOf("buggati"))
+// document.write("<br>")
+
+// document.write("car at first index of the array :" +cars[0])
+// document.write("<br>")
+
+// document.write("last index of the array :" +cars.indexOf("grande"))
+// document.write("<br>")
+
+// document.write("car at last index of the array :" +cars[4])
+// done 
+
+
+// Q.11
+
+// var students = ["HASAN" , "ahsan" , "tayyab"]
+// var totalMarks = 500
+// var obtained = [320, 490, 410];
+// var percentage = obtained / totalMarks * 100;
+
+// document.write("THE SCORE OF" +" " +students[0] +" " +"IS" +" " +obtained[0] +" " +"PERCENTAGE" +" " +percentage)
+// not done 
+
+
+// Q.12
+// not done 
+
+
+
+// Q.13
+
+// var scores = [320,230,480,180];
+// document.write(scores.sort())
+// done 
+
+
+
+// Q.14
+
+// var fruits = ["banana" , "apple" , "mango" , "watermelon" ,"strawberyy"]
+// fruits.sort()
+// document.write(fruits)
+// done 
+
+// Q.15
+
+// var cities = ["KARACHI", "ISLAMABAD" , "LAHORE" , "QUETTA" , "PESHAWAR"]
+// var selectedCities = []
+
+// selectedCities.push(cities[0])
+// selectedCities.push(cities[1])
+// selectedCities.push(cities[3])
+
+// document.write(selectedCities)
+
+// done 
+
+// Q.16
+
+// var arr = ["this" , "is", "my" , "cat"]
+// document.write(arr.join(" "))
+// done 
+
+// Q.17
+
+// var devices = ["KEYBOARD" , "MOUSE" , "MONITOR", "DATA CABLES"]
+
+// document.write(devices)
+
+// document.write("<br>")
+// document.write("<br>")
+
+
+// document.write("OUT :")
+// document.write("<br>")
+// document.write(devices[0])
+// document.write("<br>")
+
+// document.write("OUT :")
+// document.write("<br>")
+// document.write(devices[1])
+// document.write("<br>")
+
+// document.write("OUT :")
+// document.write("<br>")
+// document.write(devices[2])
+// document.write("<br>")
+
+
+// document.write("OUT :")
+// document.write("<br>")
+// document.write(devices[3])
+
+//  done 
+
+
+// Q.18
+
+
+// var devices = ["KEYBOARD" , "MOUSE" , "MONITOR", "DATA CABLES"]
+
+// document.write(devices)
+
+// document.write("<br>")
+// document.write("<br>")
+
+
+// document.write("OUT :")
+// document.write("<br>")
+// document.write(devices[3])
+// document.write("<br>")
+
+// document.write("OUT :")
+// document.write("<br>")
+// document.write(devices[2])
+// document.write("<br>")
+
+// document.write("OUT :")
+// document.write("<br>")
+// document.write(devices[1])
+// document.write("<br>")
+
+
+// document.write("OUT :")
+// document.write("<br>")
+// document.write(devices[0])
+
+// done 
+
+// Q.19
+
+// var phones = ["APPLE" , "REALME" , "OPPO" , "SAMSUNG" , "MOTOROLA"];
+// document.write("<select>");
+// for(let i = 0 ; i < phones.length ; i++){
+//     document.write("<option>" + phones[i] + "</option>");
+// }
+// document.write("</select>")
+
+// done 
+
+// Q.20
+
+// var multipleArray = [[]]
+// done 
+
+// Q.21
+
+// var matrix = [[0,1,2,3],[1,0,1,2],[2,1,0,2]];
+// document.write(matrix[0])
+// document.write("<br>")
+// document.write(matrix[1])
+// document.write("<br>")
+// document.write(matrix[2])
+
+// done 
+
+
+
